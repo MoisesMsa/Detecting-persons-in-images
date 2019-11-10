e658f27ffbe7a8d406d4493703cf516ecb9669cc
moita <moises78zito@hotmail.com>
2019-11-10 17:43:29 -0300

    dataset files

---
+++ src/model/Dataset.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.io.FileReader;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Dataset{
+
+    public Dataset() {
+        try{
+            this.readFile();
+        }catch(Exception e){
+            System.out.println("Error reading the file.");
+        }
+    }
+
+    public void readFile() throws IOException {
+
+        String row;
+        int countLine = 0;
+
+        BufferedReader csv = new BufferedReader(new FileReader(this.getPath()));
+
+        while ((row = csv.readLine()) != null) {
+            if(countLine > 0) {
+                String[] r = row.split(",");
+                this.data.add(r);
+            }
+            countLine++;
+        }
+
+        csv.close();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public List<String[]> getData() {
+        return data;
+    }
+
+    private
+        final String path = System.getProperty("user.dir")+"/dataset/dataset.csv";
+        List<String[]> data = new <String[]> ArrayList();
+}
